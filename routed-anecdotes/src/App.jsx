90c1c8e503299c9d74f5daca1bd0a6dc90e7e124
A. Urquiola <patchamama.go@gmail.com>
2023-12-28 00:26:43 +0100

    solution 7.3: routed anecdotes, step3

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -154,10 +154,15 @@ const App = () => {
   ])
 
   const [notification, setNotification] = useState('')
+  const navigate = useNavigate()
 
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
-    setAnecdotes(anecdotes.concat(anecdote))
+    setAnecdotes(anecdotes.concat(anecdote)), navigate('/')
+    setNotification(`a new anecdote ${anecdote.content} created!`)
+    setTimeout(() => {
+      setNotification('')
+    }, 10000)
   }
 
   const anecdoteById = (id) => anecdotes.find((a) => a.id === Number(id))
@@ -183,6 +188,7 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
+      {notification}
       <Routes>
         <Route
           path='/anecdotes/:id'
